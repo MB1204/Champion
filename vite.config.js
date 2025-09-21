301f54a322b78f0b3507dad5c485a30ec36a8b01
MB1204 <134436108+MB1204@users.noreply.github.com>
2025-09-21 01:04:49 -0400

    uip

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: 'all' // allows all hosts during dev
-  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') // '@' points to ./src
+      '@': path.resolve(__dirname, './src')
     },
     extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
   },
+  server: {
+    allowedHosts: 'all'
+  },
   optimizeDeps: {
     esbuildOptions: {
-      loader: {
-        '.js': 'jsx' // ensures JS files with JSX are handled correctly
-      }
+      loader: { '.js': 'jsx' }
     }
   },
   build: {
-    target: 'esnext', // optional, ensures modern JS output
-    outDir: 'dist',   // default output folder
+    outDir: 'dist'
   }
 })
